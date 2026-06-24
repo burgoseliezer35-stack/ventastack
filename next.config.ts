082c1448df0c9740552551bcb8a0e83e37b655de
burgoseliezer35-stack <burgoseliezer35@gmail.com>
2026-06-23 19:12:11 -0600

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const csp = [
   "default-src 'self'",
   "script-src 'self' 'unsafe-inline'",
   "style-src 'self' 'unsafe-inline'",
-  "img-src 'self' data: https://*.tile.openstreetmap.org https://images.openfoodfacts.org https://static.openfoodfacts.org",
+  "img-src 'self' data: blob: https://*.tile.openstreetmap.org https://images.openfoodfacts.org https://static.openfoodfacts.org https://*.supabase.co https://*.openbeautyfacts.org https://*.upcitemdb.com",
   "connect-src 'self' https://*.supabase.co",
   "font-src 'self' data:",
   "object-src 'none'",
